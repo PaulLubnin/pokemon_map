4617d2088c9073b55ccb09540357a64473a75fe7
paulego <egopaul@inbox.ru>
2022-10-22 18:18:57 +0500

    Used get_object_or_404().

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,5 @@
 import folium
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 
 from pokemon_entities.models import Pokemon, PokemonEntity
@@ -56,10 +55,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
-    except Pokemon.DoesNotExist:
-        return HttpResponseNotFound(f'<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     active_pokemons = pokemon.entities_pokemons.filter(
         pokemon_id=pokemon.id,
